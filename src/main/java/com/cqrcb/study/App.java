52e6d63d4c62f69a571fe253a7f5876e40fb3824
robin <robin@dev.cqrcb.com>
2016-04-28 16:19:09 +0800

    add to a bug to test findbugs

--- src/main/java/com/cqrcb/study/App.java
+++ src/main/java/com/cqrcb/study/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Short a = 127;
+        Short b = 127;
+        if(a == b)
+        	System.out.println("equal");
+        else
+        	System.out.println("unequal");
     }
 }
